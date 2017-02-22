f07b68d01996563d0065fc460ff9907447a5519d
tsukiyama.hiroshi <Tsukiyama.Hiroshi@moneyforward.co.jp>
2017-02-22 10:55:53 +0900

    Initial Commit

---
+++ Example/Pods/Target Support Files/SampleLib/SampleLib-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SampleLibVersionNumber;
+FOUNDATION_EXPORT const unsigned char SampleLibVersionString[];
+
---
+++ SampleLib/Classes/SampleClass.swift
@@ -0,0 +1,9 @@
+//
+//  SampleClass.swift
+//  Pods
+//
+//  Created by 築山 博 on 2017/02/22.
+//
+//
+
+import Foundation
